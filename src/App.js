38721b35d237b152e8e6386d87639230c8411e84
Yash Shelar <yshelar@asu.edu>
2022-02-14 02:30:18 -0700

    added the Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import './App.css';
+import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
     //BEM
     <div className="app">
       <h1>Hello there</h1>
+      <Header />
+      {/* Home */}
     </div>
   );
 }
