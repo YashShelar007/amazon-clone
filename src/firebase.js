b671690be650caf62e8bbea4da832fe5dd6fe5d9
Yash Shelar <yshelar@asu.edu>
2022-02-14 02:03:38 -0700

    Added firebase

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB9QeXSxdwOOV5vz1cWzI-sxqU3TWcDNCs",
+    authDomain: "clone-challenge-b0c53.firebaseapp.com",
+    projectId: "clone-challenge-b0c53",
+    storageBucket: "clone-challenge-b0c53.appspot.com",
+    messagingSenderId: "995426234832",
+    appId: "1:995426234832:web:af853723f2a672d9dd89d5",
+    measurementId: "G-KMQFM2LDXM"
+  };
