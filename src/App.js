9f5c4970266967c1aecc9e34121bb901fad47d15
Yash Shelar <yshelar@asu.edu>
2022-02-14 17:05:54 -0700

    Added Routing and started checkout Page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     //BEM
-    <div className="app">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Routes>
+          <Route path="/checkout" element={<Header />} />
+          <Route path="/" element={[<Header />, <Home />]} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
