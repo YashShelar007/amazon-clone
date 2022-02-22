2cd51113bbcd305f08bea947f8a6dc3871ce01b2
Yash Shelar <yshelar@asu.edu>
2022-02-22 00:52:11 -0700

    starting subtotal

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Subtotal.css";
 import CurrencyFormat from "react-currency-format";
 
 function Subtotal() {
@@ -8,7 +9,7 @@ function Subtotal() {
         renderText={(value) => (
           <>
             <p>
-              Subtotal (0 items):
+              Subtotal (0 items)
               <strong>0</strong>
             </p>
             <small className="subtotal__gift">
@@ -17,7 +18,7 @@ function Subtotal() {
           </>
         )}
         decimalScale={2}
-        value={getBasketToatl(basket)}
+        value={0}
         displayType={"text"}
         thousandSeparator={true}
         prefix={"$"}
