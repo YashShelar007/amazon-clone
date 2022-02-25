0df85a0409900dab606e6aafe632072790ab145e
Yash Shelar <yshelar@asu.edu>
2022-02-24 19:17:42 -0700

    Started working on basket functionality

--- src/Home.js
+++ src/Home.js
@@ -22,13 +22,13 @@ function Home() {
           <Product
             title="Samsung Galaxy Buds Pro"
             price={199.99}
-            image="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg"
+            image="https://m.media-amazon.com/images/I/51TkK-s68qS._AC_SL1000_.jpg"
             rating={4}
           />
           <Product
-            title="Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric"
-            price={98.99}
-            image="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg"
+            title="Apple Watch Series 7 | GPS + Cellular, 41mm Silver Stainless Steel Case"
+            price={498.99}
+            image="https://m.media-amazon.com/images/I/71-i7pbrhnL._AC_SL1500_.jpg"
             rating={5}
           />
         </div>
@@ -37,7 +37,7 @@ function Home() {
           <Product
             title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor"
             price={729.99}
-            image="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg"
+            image="https://m.media-amazon.com/images/I/61SQz8S+fEL._AC_SL1000_.jpg"
             rating={4}
           />
         </div>
@@ -46,14 +46,14 @@ function Home() {
           <Product
             title="New Apple iPad Pro (12.9-inch, Wi-Fi, 128GB) - Silver (4th Generation)"
             price={598.99}
-            image="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg"
+            image="https://images-na.ssl-images-amazon.com/images/I/816ctt5WV5L._AC_SX385_.jpg"
             rating={5}
           />
           <Product
-            title="The lean startup"
-            price={29.99}
-            image="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg"
-            rating={3}
+            title="Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric"
+            price={39.99}
+            image="https://m.media-amazon.com/images/I/6182S7MYC2L._AC_SL1000_.jpg"
+            rating={4}
           />
         </div>
       </div>
